586fd4704d2e10d77daa0929056ecffe81f706e3
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-04-14 20:58:05 +0900

    add function to dig blocks without tools

--- src/main/java/ryoryo/miningbelt/event/MiningEvent.java
+++ src/main/java/ryoryo/miningbelt/event/MiningEvent.java
@@ -1,35 +1,43 @@
 package ryoryo.miningbelt.event;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import ryoryo.miningbelt.item.ItemMiningBelt;
-import ryoryo.polishedlib.util.Utils;
 
 public class MiningEvent
 {
 	@SubscribeEvent
 	public void onMiningEvent(BlockEvent.BreakEvent event)
 	{
-		EntityPlayer player = event.getPlayer();
+	}
+
+	@SubscribeEvent
+	public void adjustMiningSpeed(PlayerEvent.BreakSpeed event)
+	{
+		EntityPlayer player = event.getEntityPlayer();
+		IBlockState target = event.getState();
 
 		if(ItemMiningBelt.isBeltEquipped(player))
 		{
-			Utils.addChat(player, "equipped!");
+//			event.setNewSpeed(Items.DIAMOND_PICKAXE.getDestroySpeed(new ItemStack(Items.DIAMOND_PICKAXE), target));
+			event.setNewSpeed(12f);
 		}
 	}
 
 	@SubscribeEvent
-	public void onMining(BreakSpeed event)
+	public void allowMining(PlayerEvent.HarvestCheck event)
 	{
+		EntityPlayer player = event.getEntityPlayer();
+		IBlockState target = event.getTargetBlock();
+		Block targetBlock = target.getBlock();
 
-	}
-
-	@SubscribeEvent
-	public void onMiningEmpty(PlayerEvent.HarvestCheck event)
-	{
-
+		if(ItemMiningBelt.isBeltEquipped(player))
+		{
+			event.setCanHarvest(true);
+		}
 	}
 }
